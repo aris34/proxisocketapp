02165c028978528e64c75d9f740e662602da985f
aris34 <aris.malapaschas@gmail.com>
2015-12-10 11:46:39 +0200

    on f2fInit

--- proxisocketapp/sockets/index.js
+++ proxisocketapp/sockets/index.js
@@ -1,13 +1,25 @@
 module.exports = function (io) { 
 
 	var request = require('request');
+	var users = {};
 
 	io.on('connection', function(socket){
-		console.log('a user connected');
+		console.log('a user connected ' + socket.id);
+
+		// When a client is connected, add the client to the users list,
+		if(users[msg] == null) {
+			console.log("Is null");
+			users[msg] = socket.id;
+		}
+		else {
+			console.log("Is not null");
+			io.to(socket.id).emit('chat message', 'You are already connected');
+		}
 
 		socket.on('chat message', function(msg){
-			console.log('chat message: ' + msg.sender);
-			io.emit('chat message', msg);
+			//console.log('chat message: ' + msg.sender);
+			console.log('message: ' + msg );
+			io.emit('chat message', msg);		
 
 			// if(msg === 'add user') {
 
@@ -19,6 +31,15 @@ module.exports = function (io) {
 			//     });
 			// }
 
+			// for (var i in users) {
+			// 	console.log("user " + msg +": " + users[i]);
+			// }
+			console.log(users);
+			// if(users.size > 0)
+		});
+
+		socket.on('f2fInit', function(msg){
+			console.log('f2fInit: ' + msg );
 		});
 
 		socket.on('from server', function(msg){
@@ -26,8 +47,13 @@ module.exports = function (io) {
 		});
 
 		socket.on('disconnect', function(){
-			console.log('user disconnected\n');
+			console.log('user disconnected' + socket.id);
+			for(var i in users){
+				if(users[i] == socket.id) {
+					delete users[i];
+					break;
+				}
+			}
 		});
 	});
-
 }
